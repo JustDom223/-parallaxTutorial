e342d6f191a192646ec47ccd41316069e2fc5d74
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2023-10-31 12:35:15 +0100

    I am following a tutorial to make a dynamic background.

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const parallax_el = document.querySelectorAll('.parallax');
+
+let xValue = 0,
+    yValue = 0;
+
+window.addEventListener('mousemove', (e) => {
+    xValue = e.clientX - window.innerWidth / 2;
+    yValue = e.clientY - window.innerHeight / 2;
+
+    parallax_el.forEach(el => {
+        el.style.transform = 'translate(-50%, -50%)'
+    })
+})
